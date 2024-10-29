e7453ae07bff64b11e229ee14cf0ad27eb237902
Sérgio <108956147+serjaobd@users.noreply.github.com>
2024-10-29 20:33:19 -0300

    Add files via upload

--- projeto/view/Home.java
+++ projeto/view/Home.java
@@ -1,6 +1,10 @@
 package view;
 import controller.Cadastro;
 import controller.Login;
+import model.Hoteis;
+import model.Hotel;
+import model.SistemaReserva;
+
 import java.util.Scanner;
 
 public class Home {
@@ -28,10 +32,36 @@ public class Home {
 		
 			if (opcao.equals("2")) {
 				login.logar();
+				System.out.println("1-Buscar hotel\n2-Filtrar hoteis\n3-Visualizar toda a lista de hóteis.");
+				String escolha = scanner.nextLine();
+				Hotel hotel = new Hotel("São Bernado", "Rua 1 de A", 3);
+				Hotel hotel2 = new Hotel("São Cristo", "Rua 2 de B", 4);
+				Hotel hotel3 = new Hotel("São Pedro", "Rua 3 de C", 5);
+				Hoteis hoteis = new Hoteis();
+				
+				hoteis.adicionarHotel(hotel);
+				hoteis.adicionarHotel(hotel2);
+				hoteis.adicionarHotel(hotel3);
+				
+				if (escolha.equals("1")) {
+					System.out.println("Digite o nome do hotel: ");
+					String nomeHotel = scanner.nextLine();
+					hoteis.buscarHotelPorNome(nomeHotel);
+				}else if(escolha.equals("2")) {
+					System.out.println("Filtrar hoteis");
+					hoteis.filtrarHoteisPorClassificacao(4);
+				}else if(escolha.equals("3")) {
+					hoteis.listarTodos();
+				}
+				
+				SistemaReserva sistemaReserva = new SistemaReserva();
+				sistemaReserva.iniciar();
+				scanner.close();
+			}
 			}
 		 
-		scanner.close();
+		
 	}
 	
 
-}
+
