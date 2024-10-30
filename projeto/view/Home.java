082bdaf9f98d20557ad2fa9da93c2459cd96afc9
RangellMonteiro <rangell_monteiro@hotmail.com>
2024-10-30 19:45:40 -0300

    Criação da classe main utilizando padrão Singleton

--- projeto/view/Home.java
+++ projeto/view/Home.java
@@ -1,21 +1,41 @@
 package view;
+
 import controller.Cadastro;
 import controller.Login;
+import controller.ReservaController;
 import model.Hoteis;
 import model.Hotel;
-import model.SistemaReserva;
+import controller.*;
 
 import java.util.Scanner;
 
 public class Home {
 
-    public static void main(String[] args) {
+    // Instância única da classe Home conforme o padrão singleton
+    private static Home instanciaUnica;
+
+    // Construtor privado para evitar criação de múltiplas instâncias
+    private Home() {
+    }
+
+    // Método estático que retorna a única instância da classe
+    public static Home getInstance() {
+        if (instanciaUnica == null) {
+            instanciaUnica = new Home();
+        }
+        return instanciaUnica;
+    }
+
+    // Método principal que integra as funcionalidades de cadastro, login e busca de
+    // hotéis
+    public void iniciar() {
         Scanner scanner = new Scanner(System.in);
         Cadastro cadastro = new Cadastro();
         Login login = new Login();
 
-        System.out.println("1-Cadastro\n" + "2-Login");
+        System.out.println("1-Cadastro\n2-Login");
         String opcao = scanner.nextLine();
+
         while (opcao.equals("1")) {
             System.out.println("Por favor, digite seu email");
             cadastro.setEmail(scanner.nextLine());
@@ -23,38 +43,47 @@ public class Home {
             cadastro.setSenha(scanner.nextLine());
             cadastro.cadastrar();
 
-            System.out.println("1-Cadastrar novo usuário\n" + "2-Logar");
+            System.out.println("1-Cadastrar novo usuário\n2-Logar");
             opcao = scanner.nextLine();
         }
 
         if (opcao.equals("2")) {
             login.logar();
+            Hoteis hoteis = new Hoteis();
+            hoteis.adicionarHotel(new Hotel("São Bernado", "Rua 1 de A", 3));
+            hoteis.adicionarHotel(new Hotel("São Cristo", "Rua 2 de B", 4));
+            hoteis.adicionarHotel(new Hotel("São Pedro", "Rua 3 de C", 5));
+
             System.out.println("1-Buscar hotel\n2-Filtrar hoteis\n3-Visualizar toda a lista de hotéis.");
             String escolha = scanner.nextLine();
-            
-            Hotel hotel = new Hotel("São Bernado", "Rua 1 de A", 3);
-            Hotel hotel2 = new Hotel("São Cristo", "Rua 2 de B", 4);
-            Hotel hotel3 = new Hotel("São Pedro", "Rua 3 de C", 5);
-            Hoteis hoteis = new Hoteis();
 
-            hoteis.adicionarHotel(hotel);
-            hoteis.adicionarHotel(hotel2);
-            hoteis.adicionarHotel(hotel3);
-
-            if (escolha.equals("1")) {
-                System.out.println("Digite o nome do hotel: ");
-                String nomeHotel = scanner.nextLine();
-                hoteis.buscarHotelPorNome(nomeHotel);
-            } else if (escolha.equals("2")) {
-                System.out.println("Filtrar hoteis");
-                hoteis.filtrarHoteisPorClassificacao(4);
-            } else if (escolha.equals("3")) {
-                hoteis.listarTodos();
+            switch (escolha) {
+                case "1":
+                    System.out.println("Digite o nome do hotel: ");
+                    String nomeHotel = scanner.nextLine();
+                    hoteis.buscarHotelPorNome(nomeHotel);
+                    break;
+                case "2":
+                    System.out.println("Filtrar hotéis");
+                    hoteis.filtrarHoteisPorClassificacao(4);
+                    break;
+                case "3":
+                    hoteis.listarTodos();
+                    break;
+                default:
+                    System.out.println("Opção inválida.");
+                    break;
             }
 
-            SistemaReserva sistemaReserva = new SistemaReserva();
-            sistemaReserva.iniciar();
-            scanner.close();
+            ReservaController reservaController = new ReservaController();
+            reservaController.iniciar();
         }
+
+        scanner.close();
+    }
+
+    public static void main(String[] args) {
+        // Chama o método para iniciar a única instância da aplicação
+        Home.getInstance().iniciar();
     }
 }
